aeec417d44b49a0ac690dfa05467cbbfdee793a1
ChrixWX92 <chris@wards-net.co.uk>
2022-05-11 10:38:35 +0100

    Latest

--- src/main/java/ru/nukkitx/forms/SimpleFormResponse.java
+++ src/main/java/ru/nukkitx/forms/SimpleFormResponse.java
@@ -1,9 +1,9 @@
 package ru.nukkitx.forms;
 
 import cn.nukkit.Player;
-import cn.nukkit.form.window.FormWindow;
 import cn.nukkit.form.window.FormWindowSimple;
 
+@FunctionalInterface
 public interface SimpleFormResponse extends FormResponse {
 
     void handle(Player targetPlayer, FormWindowSimple targetForm, int data);
